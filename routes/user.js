4da6ca8745035a539fa589db97f97b07f6bb5353
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-10-01 08:16:56 +0530

    remove Comments

--- routes/user.js
+++ routes/user.js
@@ -1,79 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const Survey = require('../models/Survey');
-const checkAuthentication = require('../middleware/checkAuthentication');
-const checkAuthorization = require('../middleware/checkAuthorization');
-const moment = require('moment');
+const User = require("../models/User");
+const Survey = require("../models/Survey");
+const checkAuthentication = require("../middleware/checkAuthentication");
+const checkAuthorization = require("../middleware/checkAuthorization");
+const moment = require("moment");
 
+// Just To remove Comments
 
 // User Dashboard
-router.get('/dashboard', checkAuthentication, async (req, res) => {
-    try {
-        const surveys = await Survey.find({user: req.user}).sort({createdAt: -1});
-        res.render('main/dashboard', {user: req.user, surveys});
-    } catch (e) {
-        console.log(e);
-        res.json({error: 'Server Error'});
-    }
+router.get("/dashboard", checkAuthentication, async (req, res) => {
+  try {
+    const surveys = await Survey.find({ user: req.user }).sort({
+      createdAt: -1,
+    });
+    res.render("main/dashboard", { user: req.user, surveys });
+  } catch (e) {
+    console.log(e);
+    res.json({ error: "Server Error" });
+  }
 });
 
 // My Surveys
-router.get('/survey/:id', async (req, res) => {
-    try {
-        const surveys = await Survey.find({user: req.params.id}).sort({createdAt: -1}).populate('user').populate('surveyQs').exec();
-        const user = await User.findById(req.params.id);
-        res.render('main/public', {surveys, user: req.user, owner: user});
-    } catch (e) {
-        console.log(e);
-        res.json({error: 'Server Error'});
-    }
+router.get("/survey/:id", async (req, res) => {
+  try {
+    const surveys = await Survey.find({ user: req.params.id })
+      .sort({ createdAt: -1 })
+      .populate("user")
+      .populate("surveyQs")
+      .exec();
+    const user = await User.findById(req.params.id);
+    res.render("main/public", { surveys, user: req.user, owner: user });
+  } catch (e) {
+    console.log(e);
+    res.json({ error: "Server Error" });
+  }
 });
 
 // Survey Create
-router.post('/survey', checkAuthentication, async (req, res) => {
-    const {title} = req.body;
-    try {
-        let newSurvey = new Survey({
-            title,
-            user: req.user,
-            showDate: moment(new Date()).format("MMMM Do YYYY")
-        });
-        newSurvey = await newSurvey.save();
-        req.flash('success', 'Survey created Successfully');
-        res.redirect('back');
-    } catch (e) {
-        console.log(e);
-        req.flash('error', 'Sorry!! Server Error occurred');
-        res.redirect('back');
-    }
+router.post("/survey", checkAuthentication, async (req, res) => {
+  const { title } = req.body;
+  try {
+    let newSurvey = new Survey({
+      title,
+      user: req.user,
+      showDate: moment(new Date()).format("MMMM Do YYYY"),
+    });
+    newSurvey = await newSurvey.save();
+    req.flash("success", "Survey created Successfully");
+    res.redirect("back");
+  } catch (e) {
+    console.log(e);
+    req.flash("error", "Sorry!! Server Error occurred");
+    res.redirect("back");
+  }
 });
 
 // Survey Update
-router.put('/survey/:id', checkAuthorization, async (req, res) => {
-    try{
-        const {title} = req.body;
-        await Survey.updateOne({_id: req.params.id}, {title});
-        req.flash('success', 'Survey title Updated');
-        res.redirect('back');
-    }catch(e){
-        console.log(e);
-        req.flash('error', 'Sorry!! Server Error occurred');
-        res.redirect('back');
-    }
+router.put("/survey/:id", checkAuthorization, async (req, res) => {
+  try {
+    const { title } = req.body;
+    await Survey.updateOne({ _id: req.params.id }, { title });
+    req.flash("success", "Survey title Updated");
+    res.redirect("back");
+  } catch (e) {
+    console.log(e);
+    req.flash("error", "Sorry!! Server Error occurred");
+    res.redirect("back");
+  }
 });
 
 // Survey Delete
-router.delete('/survey/:id', checkAuthorization, async (req, res) => {
-    try{
-        await Survey.findByIdAndDelete(req.params.id);
-        req.flash('success', 'Survey deleted Successfully');
-        res.redirect('back');
-    }catch(e){
-        console.log(e);
-        req.flash('error', 'Sorry!! Server Error occurred');
-        res.redirect('back');
-    }
+router.delete("/survey/:id", checkAuthorization, async (req, res) => {
+  try {
+    await Survey.findByIdAndDelete(req.params.id);
+    req.flash("success", "Survey deleted Successfully");
+    res.redirect("back");
+  } catch (e) {
+    console.log(e);
+    req.flash("error", "Sorry!! Server Error occurred");
+    res.redirect("back");
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const config = require('config');
-const connectDB = require('./config/db');
-const passportConfig = require('./config/passport');
-const methodOverride = require('method-override');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const config = require("config");
+const connectDB = require("./config/db");
+const passportConfig = require("./config/passport");
+const methodOverride = require("method-override");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const rateLimit = require("express-rate-limit");
-const cors = require('cors');
-
+const cors = require("cors");
 
 // Database Connect
 connectDB();
 
 // Body Parser Setup
-app.use(express.urlencoded({
-    extended: true
-}));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 // Security
 app.use(mongoSanitize());
@@ -32,45 +33,46 @@ app.use(helmet());
 app.use(xss());
 app.use(hpp());
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 50
+  windowMs: 10 * 60 * 1000,
+  max: 50,
 });
 app.use(limiter);
 app.use(cors());
 
 // Session Setup
-app.use(session({
+app.use(
+  session({
     resave: false,
-    secret: config.get('sessionSecret'),
-    saveUninitialized: false
-}));
+    secret: config.get("sessionSecret"),
+    saveUninitialized: false,
+  })
+);
 
 // Connect Flash
 app.use(flash());
 app.use((req, res, next) => {
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-// Passport Setup 
+// Passport Setup
 passportConfig(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('main/index', {
-        user: req.user
-    });
+app.get("/", (req, res) => {
+  res.render("main/index", {
+    user: req.user,
+  });
 });
-app.use('/survey', require('./routes/survey'));
-app.use('/auth', require('./routes/auth'));
-app.use('/users', require('./routes/user'));
-
+app.use("/survey", require("./routes/survey"));
+app.use("/auth", require("./routes/auth"));
+app.use("/users", require("./routes/user"));
 
 // PORT listen
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+app.listen(PORT, process.env.IP, () => {
+  console.log(`Server started on port ${PORT}`);
 });
